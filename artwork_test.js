a7217e118316d1338e0c5463e7dc5440835a0869
Tashalia Nair <33638169+novacaine98@users.noreply.github.com>
2021-12-10 17:28:48 +0200

    fixed some typos and also added all the proper text for the game

--- artwork_test.js
+++ artwork_test.js
@@ -42,81 +42,175 @@ function selectOption(option) {
 const textNodes = [
   {
     id: 1,
-    text: 'Hello and welcome to the Prototype tour!! This is just to test out some functionality and to see what the very bare version of this game will look like!',
+    text: 'i hate looking in the mirror. i always feel like a stranger is looking back at me',
     options: [
       {
-        text: 'Alright!',
+        text: 'lost',
         
         nextText: 2
       },
       {
-        text: 'Sweet',
-        nextText: 2
+        text: 'Denial',
+        nextText: 3
       }
     ]
   },
   {
     id: 2,
-    text: 'So, over here we have...nothing much but it will be something soon! I hope to have a cool illustration or GIF above this text at some point. Stay tuned for that!',
+    text: 'maybe. i used to think i knew myself. i thought i knew what i liked and what i did not. i used to think i could recognise myself.',
     options: [
       {
-        text: 'oooh pretty pictures!',
-        nextText: 3
+        text: 'Who is she?',
+        nextText: 4
       },
       {
-        text: 'I would like to see it, yes.',
-        nextText: 3
+        text: 'Who are you?',
+        nextText: 5
       }
     ]
   },
   {
     id: 3,
-    text: 'I really hope this game works out! I think it would be really cool to see it in all its glory at the end of this',
+    text: 'i am Not. i really do not know her...them?...me?',
     options: [
       {
-        text: 'I hope it works out for you too',
+        text: 'Who is she?',
         nextText: 4
       },
       {
-        text: 'Better get working then!',
+        text: 'Who are you?',
         nextText: 5
-      },
-      {
-        text: 'good luck!!',
-        nextText: 6
       }
     ]
   },
   {
     id: 4,
-    text: 'Alright! Last stop on the tour! Thanks for visiting, I hope it was not too disappointing and you can see (if you squint your eyes and tilt your head a little) the vision for this game!',
+    text: 'i do not know but she does not look happy...she...she...she?',
     options: [
       {
-        text: 'Thanks for the trip!',
+        text: 'You.',
         nextText: -1
       }
     ]
   },
   {
     id: 5,
-    text: 'Alright! Last stop on the tour! Thanks for visiting, I hope it was not too disappointing and you can see (if you squint your eyes and tilt your head a little) the vision for this game!',
+    text: 'an age old question. i do not know...there is a line from this book called "The Song of Achilles" where Patroclus answers in response to a similar question "I am made of memories"...maybe that is what i am?',
     options: [
       {
-        text: 'See you soon!',
-        nextText: -1
+        text: 'Ghost',
+        nextText: 6
+      },
+      {
+        text: 'Shell',
+        nextText: 7
       }
     ]
   },
   {
     id: 6,
-    text: 'Alright! Last stop on the tour! Thanks for visiting, I hope it was not too disappointing and you can see (if you squint your eyes and tilt your head a little) the vision for this game!',
+    text: 'drifting aimlessly, not knowing where to go or what to do. lost. sounds about right...i think i have only ever been what others needed',
     options: [
       {
-        text: 'Cannot wait to see how it turns out!',
-        nextText: -1
+        text: 'Daughter',
+        nextText: 8
+      },
+      {
+        text: 'Sister',
+        nextText: 9
       }
     ]
-  }
+  },
+  {
+    id: 7,
+    text: 'a shell...maybe. i do not think i have ever been anything other than what others have wanted...i think i am scared of who i would be if i was not.',
+    options: [
+      {
+        text: 'Daughter',
+        nextText: 8
+      },
+      {
+        text: 'Sister',
+        nextText: 9
+      }
+    ]
+  },
+  {
+    id: 8,
+    text: 'the thing that brought so many problems. always be good and behave calmly. always be level-headed, do not give them a reason to worry or stress. do not make them regret it. prove that you are worth it.',
+    options: [
+      {
+        text: 'Sister',
+        nextText: 9
+      },
+      {
+        text: 'Get over it.',
+        nextText: 10
+      }
+    ]
+  },
+  {
+    id: 9,
+    text: 'protector. parent. second-best. friend. i love being an older sister, i really do. but i think...i think i am tired of being second-best. i think i am tired of not being good enough...tired of never knowing if i was made for me or made for them.',
+    options: [
+      {
+        text: 'Daughter',
+        nextText: 8
+      },
+      {
+        text: 'Cry me a river.',
+        nextText: 10
+      }
+    ]
+  },
+  {
+    id: 10,
+    text: 'maybe you have a point...',
+    options: [
+      {
+        text: 'Weak',
+        nextText: 11
+      },
+      {
+        text: 'Selfish',
+        nextText: 11
+      }
+    ]
+  },
+  {
+    id: 11,
+    text: 'i am not i am just-',
+    options: [
+      {
+        text: 'WEAK',
+        nextText: 12
+      },
+      {
+        text: 'SELFISH',
+        nextText: 12
+      }
+    ]
+  },
+  {
+    id: 12,
+    text: 'I AM NOT I JUST-',
+    options: [
+      {
+        text: 'JUST WHAT????',
+        nextText: 13
+      },
+    ]
+  },
+  {
+    id: 13,
+    text: 'i just-',
+    options: [
+      {
+        text: 'Spit. It. Out.',
+        nextText: -1
+      },
+    ]
+  },
 ]
 
 startGame()
